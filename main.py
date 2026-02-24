666ff77b6dfbe5153c31a3e8932f204408a16c78
mare mdma <vid.vujisic@sts.sckr.si>
2026-02-24 10:52:59 +0100

    Add files via upload
    skoraj narejena vaja

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/coinFlip")
+def coinFlip():
+    return render_template("coinFlip.html")
+
+@app.route("/coinData")
+def coinData():
+   # print(request.args["vrednost"])
+    rnd = random.randint(0,1)
+    status = ["HEADS", "TAILS"][rnd]
+    return {"img" : "url", "status" : status}
+
+app.run(debug = True)
